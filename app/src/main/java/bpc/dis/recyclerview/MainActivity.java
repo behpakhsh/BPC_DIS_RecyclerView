4e597b85379e55b24227f8fcb3c12c663d39bda2
hossein sohrabi fard <hossein.sohrabi.94@gmail.com>
2019-10-06 15:01:36 +0330

    initialize

--- app/src/main/java/bpc/dis/recyclerview/MainActivity.java
+++ app/src/main/java/bpc/dis/recyclerview/MainActivity.java
@@ -4,22 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<String> strings = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
-            strings.add("test item: " + i);
-        }
-//        TestAdapter testAdapter = new TestAdapter(this, strings);
-//        BpcRecyclerView bpcRecyclerView = findViewById(R.id.rv_bpc);
-//        bpcRecyclerView.setAdapter(testAdapter);
     }
 
 }
